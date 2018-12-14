58a25c25dea482c64400d05d5dbd2fe392012bdd
Jim Fleming <jimmy.fleming@gmail.com>
2018-12-14 09:14:59 -0800

    Add scripts

--- launch_instance.sh
+++ launch_instance.sh
@@ -8,8 +8,7 @@ SCOPES=default,storage-rw
 IMAGE_FAMILY=tf-latest-gpu
 IMAGE_PROJECT=deeplearning-platform-release
 
-BOOT_DISK_TYPE=pd-ssd
-BOOT_DISK_SIZE=30GB
+BOOT_DISK_NAME=$1
 
 GPU_TYPE=nvidia-tesla-k80
 GPU_COUNT=1
@@ -19,9 +18,7 @@ gcloud compute instances create $INSTANCE_NAME \
     --machine-type=$MACHINE_TYPE \
     --image-family=$IMAGE_FAMILY \
     --image-project=$IMAGE_PROJECT \
-    --boot-disk-type=$BOOT_DISK_TYPE \
-    --boot-disk-size=$BOOT_DISK_SIZE \
-    --no-boot-disk-auto-delete \
+    --disk="name=$BOOT_DISK_NAME,device-name=$BOOT_DISK_NAME,mode=rw,boot=yes" \
     --maintenance-policy=TERMINATE \
     --restart-on-failure \
     --scopes=$SCOPES \
