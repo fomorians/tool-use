43719272762fa5f89d4a9963c0337a4677bcf19b
Jim Fleming <jimmy.fleming@gmail.com>
2018-12-14 10:36:27 -0800

    Fix sync script

--- sync.sh
+++ sync.sh
@@ -2,4 +2,4 @@
 INSTANCE_NAME=$1
 gcloud compute scp --recurse . $INSTANCE_NAME:~/tool_use
 gcloud compute scp --recurse ~/Documents/pyoneer $INSTANCE_NAME:~/pyoneer
-gcloud compute scp --recurse $INSTANCE_NAME:~/jobs ~/jobs/tool_use/
+gcloud compute scp --recurse $INSTANCE_NAME:~/jobs/ ~/jobs/tool_use/
