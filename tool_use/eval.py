26196336718501e83571787ab7b594cdc4253eb6
Jim Fleming <jimmy.fleming@gmail.com>
2019-01-16 18:43:08 -0800

    Update eval

--- tool_use/eval.py
+++ tool_use/eval.py
@@ -19,31 +19,44 @@ def main():
     parser.add_argument('--render', action='store_true')
     args = parser.parse_args()
 
-    tf.enable_eager_execution()
+    # register kuka env
+    gym.envs.register(
+        id='KukaEnv-v0',
+        entry_point='tool_use.kuka_env:KukaEnv',
+        max_episode_steps=1000)
 
+    # params
     params = HyperParams(env=args.env, seed=args.seed)
 
-    env = gym.make(args.env_name)
+    # eager
+    tf.enable_eager_execution()
 
+    # environment
+    env = gym.make(args.env)
+
+    # seeding
     env.seed(args.seed)
     random.seed(args.seed)
     np.random.seed(args.seed)
     tf.set_random_seed(args.seed)
 
+    # rollouts
     rollout = Rollout(env, max_episode_steps=env.spec.max_episode_steps)
 
+    # policies
     policy = Policy(
         observation_space=env.observation_space,
         action_space=env.action_space,
         scale=params.scale)
-
     inference_strategy = pyrl.strategies.ModeStrategy(policy)
 
+    # checkpoints
     checkpoint = tf.train.Checkpoint(policy=policy)
     checkpoint_path = tf.train.latest_checkpoint(args.job_dir)
     assert checkpoint_path is not None
     checkpoint.restore(checkpoint_path)
 
+    # rollouts
     states, actions, rewards, next_states, weights = rollout(
         inference_strategy, episodes=args.episodes, render=args.render)
     episodic_reward = tf.reduce_mean(tf.reduce_sum(rewards, axis=-1))
