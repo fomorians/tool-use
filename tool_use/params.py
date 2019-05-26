1635df2f9516e46a6e687ccabbd64ac5808a680d
Jim Fleming <jimmy.fleming@gmail.com>
2019-05-26 09:13:02 -0700

    Adjust hps

--- tool_use/params.py
+++ tool_use/params.py
@@ -25,8 +25,8 @@ class HyperParams:
     # losses
     value_coef = attr.ib(default=1e-3)
     entropy_coef = attr.ib(default=1e-4)
-    forward_coef = attr.ib(default=1.0)
-    inverse_coef = attr.ib(default=1e-1)
+    forward_coef = attr.ib(default=0.2)
+    inverse_coef = attr.ib(default=0.8)
     intrinsic_coef = attr.ib(default=10.0)
     l2_coef = attr.ib(default=0.0)
 
